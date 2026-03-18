3a19cca80b89e67ee93e1bc3f3c50b76860614d1
stanislav-kidun <kruzenger@gmail.com>
2026-03-18 13:08:18 +0300

    added solution for 938. Range Sum of BST

---
+++ 938. Range Sum of BST/938. Range Sum of BST.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/range-sum-of-bst/description/
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root == nullptr) {
+            return 0;
+        }
+        
+        if(root->val < low) {
+            rangeSumBST(root->right, low, high);
+        } else if (root->val > high) {
+            rangeSumBST(root->left, low, high);
+        } else {
+            result += root->val;
+            rangeSumBST(root->right, low, high);
+            rangeSumBST(root->left, low, high);
+        }
+
+        return result;
+    }
+
+private:
+    int result;
+};
